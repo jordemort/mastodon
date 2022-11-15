2ff6f57a1db39b247d75e1cd09e9bb7df8d4eb9c
Jordan Webb <jordan@caketop.app>
2022-11-15 10:10:23 -0600

    s/Publish/Squonk/

--- app/javascript/flavours/glitch/features/compose/components/publisher.js
+++ app/javascript/flavours/glitch/features/compose/components/publisher.js
@@ -16,7 +16,7 @@ import { maxChars } from 'flavours/glitch/initial_state';
 //  Messages.
 const messages = defineMessages({
   publish: {
-    defaultMessage: 'Publish',
+    defaultMessage: 'Squonk',
     id: 'compose_form.publish',
   },
   publishLoud: {
