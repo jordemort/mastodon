a4b4e319e7395b1f961d566d8409ab72047b98ac
Ariadne Conill <ariadne@dereferenced.org>
2022-12-25 02:07:07 +0000

    models: status: add support for quoting

--- app/models/status.rb
+++ app/models/status.rb
@@ -29,6 +29,7 @@
 #  edited_at                    :datetime
 #  trendable                    :boolean
 #  ordered_media_attachment_ids :bigint(8)        is an Array
+#  quote_id                     :bigint(8)
 #
 
 class Status < ApplicationRecord
@@ -63,6 +64,7 @@ class Status < ApplicationRecord
 
   belongs_to :thread, foreign_key: 'in_reply_to_id', class_name: 'Status', inverse_of: :replies, optional: true
   belongs_to :reblog, foreign_key: 'reblog_of_id', class_name: 'Status', inverse_of: :reblogs, optional: true
+  belongs_to :quote, foreign_key: 'quote_id', class_name: 'Status', inverse_of: :quote, optional: true
 
   has_many :favourites, inverse_of: :status, dependent: :destroy
   has_many :bookmarks, inverse_of: :status, dependent: :destroy
@@ -79,6 +81,8 @@ class Status < ApplicationRecord
   has_many :local_favorited, -> { merge(Account.local) }, through: :favourites, source: :account
   has_many :local_reblogged, -> { merge(Account.local) }, through: :reblogs, source: :account
   has_many :local_bookmarked, -> { merge(Account.local) }, through: :bookmarks, source: :account
+
+  has_many :quoted, foreign_key: 'quote_id', class_name: 'Status', inverse_of: :quote, dependent: :nullify
 
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :preview_cards
@@ -170,6 +174,17 @@ class Status < ApplicationRecord
                      account: [:account_stat, user: :role],
                      active_mentions: { account: :account_stat },
                    ],
+                   quote: [
+                     :application,
+                     :tags,
+                     :preview_cards,
+                     :media_attachments,
+                     :conversation,
+                     :status_stat,
+                     :preloadable_poll,
+                     account: [:account_stat, :user],
+                     active_mentions: { account: :account_stat },
+                   ],
                    thread: { account: :account_stat }
 
   delegate :domain, to: :account, prefix: true
@@ -202,6 +217,10 @@ class Status < ApplicationRecord
 
   def reblog?
     !reblog_of_id.nil?
+  end
+
+  def quote?
+    !quote_id.nil? && quote
   end
 
   def within_realtime_window?
@@ -272,7 +291,7 @@ class Status < ApplicationRecord
     fields  = [spoiler_text, text]
     fields += preloadable_poll.options unless preloadable_poll.nil?
 
-    @emojis = CustomEmoji.from_text(fields.join(' '), account.domain)
+    @emojis = CustomEmoji.from_text(fields.join(' '), account.domain) + (quote? ? CustomEmoji.from_text([quote.spoiler_text, quote.text].join(' '), quote.account.domain) : [])
   end
 
   def ordered_media_attachments
