81101f31eb07dfac1e8ae04110e154d918c79e16
Ariadne Conill <ariadne@dereferenced.org>
2022-12-26 02:57:20 +0000

    activitypub: switch to fedibird:quoteUri
    (cherry picked from commit e59c40eb686684c353e5b322753c5022b444a92b)

--- app/helpers/context_helper.rb
+++ app/helpers/context_helper.rb
@@ -42,7 +42,7 @@ module ContextHelper
       'cipherText' => 'toot:cipherText',
     },
     suspended: { 'toot' => 'http://joinmastodon.org/ns#', 'suspended' => 'toot:suspended' },
-    quoteUrl: { 'quoteUrl' => 'as:quoteUrl' },
+    quoteUri: { 'fedibird' => 'http://fedibird.com/ns#', 'quoteUri' => 'fedibird:quoteUri' },
   }.freeze
 
   def full_context
