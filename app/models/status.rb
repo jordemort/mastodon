6bcd41b56c4d7b3b7e87c64cba8392f9b5d44072
Essem <smswessem@gmail.com>
2023-11-07 18:43:47 -0600

    Add support for emoji reactions Squashed, modified, and rebased from glitch-soc/mastodon#2221.
    Co-authored-by: fef <owo@fef.moe>
    Co-authored-by: Jeremy Kescher <jeremy@kescher.at>
    Co-authored-by: neatchee <neatchee@gmail.com>
    Co-authored-by: Ivan Rodriguez <104603218+IRod22@users.noreply.github.com>
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/models/status.rb
+++ app/models/status.rb
@@ -79,6 +79,7 @@ class Status < ApplicationRecord
   has_many :mentions, dependent: :destroy, inverse_of: :status
   has_many :mentioned_accounts, through: :mentions, source: :account, class_name: 'Account'
   has_many :media_attachments, dependent: :nullify
+  has_many :status_reactions, inverse_of: :status, dependent: :destroy
   has_many :quoted, foreign_key: 'quote_id', class_name: 'Status', inverse_of: :quote, dependent: :nullify
 
   # The `dependent` option is enabled by the initial `mentions` association declaration
@@ -309,6 +310,21 @@ class Status < ApplicationRecord
     fields += preloadable_poll.options unless preloadable_poll.nil?
 
     @emojis = CustomEmoji.from_text(fields.join(' '), account.domain) + (quote? ? CustomEmoji.from_text([quote.spoiler_text, quote.text].join(' '), quote.account.domain) : [])
+  end
+
+  def reactions(account = nil)
+    records = begin
+      scope = status_reactions.group(:status_id, :name, :custom_emoji_id).order(Arel.sql('MIN(created_at) ASC'))
+
+      if account.nil?
+        scope.select('name, custom_emoji_id, count(*) as count, false as me')
+      else
+        scope.select("name, custom_emoji_id, count(*) as count, exists(select 1 from status_reactions r where r.account_id = #{account.id} and r.status_id = status_reactions.status_id and r.name = status_reactions.name and (r.custom_emoji_id = status_reactions.custom_emoji_id or r.custom_emoji_id is null and status_reactions.custom_emoji_id is null)) as me")
+      end
+    end
+
+    ActiveRecord::Associations::Preloader.new(records: records, associations: :custom_emoji)
+    records
   end
 
   def ordered_media_attachments
